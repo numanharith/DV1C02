f96a9f6ecaae639284f29c89aae7fb66944a7a08
numan <numan.h.99@gmail.com>
2022-10-08 13:26:21 +0800

    completed vet related code

--- src/main/java/com/numan/dv1c02/petclinic/vet/Specialty.java
+++ src/main/java/com/numan/dv1c02/petclinic/vet/Specialty.java
@@ -3,8 +3,6 @@ package com.numan.dv1c02.petclinic.vet;
 import com.numan.dv1c02.petclinic.model.NamedEntity;
 
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
